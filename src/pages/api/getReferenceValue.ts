138474e4e8be404edf46139d1e4fa882e9237ac3
arnaudakotonou97 <akotonouarnaudfemi@gmail.com>
2023-06-17 00:04:13 +0100

    fix: fixed nextjs env contentful variable config

--- src/pages/api/getReferenceValue.ts
+++ src/pages/api/getReferenceValue.ts
@@ -1,8 +1,4 @@
-// https://cdn.contentful.com/spaces/9c0tqh10g15j/environments/master/entries?content_type=portfolio_page
 import contentfulClient from "./contentfulClient"
-
-// const { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } = process.env
-const CONTENTFUL_SPACE_ID = "9c0tqh10g15j"
 
 export async function getReferenceValue(refId: string, attribute?: string) {
   const refValue = contentfulClient().getEntry(refId)
